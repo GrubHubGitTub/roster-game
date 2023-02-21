06b1c1dd1fce0c3859adb6821c125839d08360f0
GrubHubGitTub <aidangruber@hotmail.com>
2023-02-21 14:39:07 +0700

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+b = pd.read_csv("Batting.csv")
+p = pd.read_csv("Pitching.csv")
+
+data = pd.concat([b,p])
+
+data = data[["playerID", "teamID"]]
+data = data.drop_duplicates()
+data = data.groupby('playerID')['teamID'].apply(list).reset_index(name='teams')
+print(data.head(10))
+
+data.to_csv("player-teams.csv", index=False)
